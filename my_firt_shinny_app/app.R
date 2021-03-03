d97b9fcdabe9b0f6d2507a9652e3a9b3e21ed767
SanaeAbm <sanae.abm@gmail.com>
2021-03-03 10:36:13 +0100

    updated_plots

--- my_firt_shinny_app/app.R
+++ my_firt_shinny_app/app.R
@@ -8,6 +8,10 @@ library(ggplot2)
 
 var_list=c("Time","Cast","Rating")
 
+Film$Good[which(Film$Good == 0)] <- 'Bad'
+Film$Good[which(Film$Good == 1)] <- 'Good'
+Film$Good <- as.factor(Film$Good)
+
 Film=Film%>% mutate(Origin = replace(Origin, Origin==0, "USA"))
 Film=Film%>% mutate(Origin = replace(Origin, Origin==1, "GB"))
 Film=Film%>% mutate(Origin = replace(Origin, Origin==2, "FR"))
@@ -21,7 +25,7 @@ ui <- navbarPage("Film data in R Shinny App",
                  tabPanel("Descriptive statistics",
                      fluidPage(
                      sidebarLayout(sidebarPanel(
-                     selectInput("select", label = h3("Univariante statistic"), 
+                     selectInput("select", label = h3("Main descriptive statistics per variable"), 
                                      choices = var_list,
                                      selected = 1)),
                     
@@ -30,24 +34,62 @@ ui <- navbarPage("Film data in R Shinny App",
                      ))),
                  
                  navbarMenu("Plot graphs",
-                            tabPanel("Graph1",
-                            fluidPage(
-                                    h3("Plots"),
-                                    plotOutput(outputId = "p1")
-                                )
+                            tabPanel("Year-time scatterplot",
+                                     basicPage(
+                                       plotOutput("p1", click = "click"),
+                                       verbatimTextOutput("info")
+                                     )
                  ),
                                 
-                            tabPanel("Graph2")),
+                            tabPanel("Time Histogram",
+                                     fluidPage(  
+                                         titlePanel( "Time Distribution Histogram" ),  
+                                          sidebarLayout( 
+                                             sidebarPanel(
+                                                 sliderInput(
+                                                      "bins",
+                                                      "Bin Width:",
+                                                      min = 5,
+                                                      max = 50,
+                                                      value = 20 )
+                                                ),
+                                             mainPanel(   
+                                                  plotOutput( "p2" )
+                                                )
+                                           )
+                                        )
+                                     )),
                  tabPanel("References",
                           includeMarkdown("references.md"))
+
 )
 
+
+
+
 server <- function(input, output){
-    dt_f <- reactive({
-        x<-as.array(summary(Film[,input$select]))
+    df <- reactive({
+        x<-as.data.frame.table(as.array(summary(Film[,input$select])))
     })
-    output$table <- renderTable(dt_f())
+    names(df)<-c("Statistic","Value")
+    output$table <- renderTable(df())
+    
+    output$p1 <- renderPlot({
+      ggplot(data =Film) + geom_point(mapping = aes(x = Year, y = Time,color=Good),size=4)+ ggtitle( "Duration per year scatterplot")
+    })
+    
+    
+    output$info <- renderText({
+      a=strtrim(input$click$x,4)
+      b= strtrim(input$click$y,5)
+      paste0("Year=",a, "\nRunning time=",b)
+    })
+    
+    output$p2 <- renderPlot( {
+           ggplot( data=Film, aes( x=Time ) ) + geom_histogram( binwidth=input$bins, colour="white", fill="lightblue", alpha=0.7 ) + scale_y_continuous( breaks=seq( 0, length( Film$Time ), by=2 ) ) + ggtitle( "Movie duration distribution" )
+        } )
 }
+
 
 
 # Run the application 
